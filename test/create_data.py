dacea5689723fe86f65f36673b4ede7ad5c89ff3
David <dguetzlaff@gmail.com>
2022-12-21 17:11:56 -0600

    Finished test of python-git functionality

--- test/create_data.py
+++ test/create_data.py
@@ -5,19 +5,25 @@ import os
 from datetime import datetime 
 
 def test_commit_file():
+    # Grab original information and determine the working directory
     original_dir = os.getcwd()
     working_dir = os.getcwd().split('alphazero')[0] + 'alphazero/play_data/test'
     os.chdir(working_dir)
 
+    # Get the current time for the commit message
     current_time = datetime.now().strftime("%D %H:%M:%S")
 
-    os.system("echo Committing a file at " + str(current_time))
-    # os.system('git add test_file.txt')
-    os.system('git status')
+    os.system("git pull")
+    # alter the file
     with open("test_file.txt", "a") as file:
         file.write('Running the test \n')
 
-    os.system("echo Created a new file?")
+    # add, commit, & push the file
+    print("Committing a file at " + str(current_time))
+    os.system('git add test_file.txt')
+    os.system('git commit -m "'+ str(current_time) + ': Running Test"')
+    os.system("git push")
+    print("File Successfully Pushed")
     os.chdir(original_dir)
 
 
